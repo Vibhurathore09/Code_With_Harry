7f8a7abf2f34724ffa65cca56b9ca0ab91bd2f76
Vibhu Rathore <79627056+Vibhurathore09@users.noreply.github.com>
2022-12-25 07:39:40 -0800

    Add files via upload

---
+++ JS/SearchingTheDom/36.js
@@ -0,0 +1,14 @@
+// Change the card tilte to red
+// let ctitle = document.getElementsByClassName("card-title")[0]
+// console.log(ctitle);
+// ctitle.style.color = "red"
+// let cid = document.getElementById("firstcardtitle")
+// console.log(cid)
+// ctitle.style.backgroundColor = "orange"
+
+
+let ctitles = document.querySelectorAll(".card-title")
+console.log(ctitles)
+ctitles[0].style.color = "blue"
+ctitles[1].style.color = "red"
+ctitles[2].style.color = "green"
