d31e3c6f598a1e83977c6f512461783d6a6c3102
Vibhu Rathore <79627056+Vibhurathore09@users.noreply.github.com>
2022-12-24 23:52:46 -0800

    Add files via upload

---
+++ JS/33/33.js
@@ -0,0 +1,5 @@
+console.log(document.body.firstChild)
+a = document.body.firstChild
+console.log(a.parentNode) //It will return any text , node or anything
+console.log(a.parentElement) // it will return if the parent is an Element
+console.log(a.firstChild.nextSibling)
