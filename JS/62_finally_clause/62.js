ea69a32ddcd81afe8263dd0c54c111d94e2e5726
Vibhu Rathore <79627056+Vibhurathore09@users.noreply.github.com>
2023-01-24 09:10:25 -0800

    Add files via upload

---
+++ JS/62_finally_clause/62.js
@@ -0,0 +1,11 @@
+try{
+    let a = 0;
+    console.log(program);
+    console.log(" Program ran Successfully");
+}
+catch (err){
+    console.log("This is an error");
+}
+finally{
+    console.log("I am a good boy")
+}
